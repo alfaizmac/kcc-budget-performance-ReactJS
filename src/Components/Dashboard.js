53c7887b30da321b23a8b71c059a92f830091115
Alfaiz Mac <macalangcomalfaiz2@gmail.com>
2025-02-07 18:18:00 +0800

    solving the button on bottom right.

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -87,18 +87,6 @@ function Dashboard() {
     <div className="dashboard">
       <h1>Overall Analytics</h1>
 
-      {/* Input for Spreadsheet URL */}
-      <div className="input-section">
-        <label>Enter Spreadsheet URL: </label>
-        <input
-          type="text"
-          value={spreadsheetUrl}
-          onChange={(e) => setSpreadsheetUrl(e.target.value)}
-          placeholder="Paste Google Spreadsheet public link here"
-        />
-        <button onClick={handleFetchData}>Fetch Data</button>
-      </div>
-
       {/* Filter Section */}
       <div className="filter-section">
         <label>Filter :</label>
@@ -150,6 +138,19 @@ function Dashboard() {
           </LineChart>
         </ResponsiveContainer>
       </div>
+
+      {/* Floating Button for Opening Modal */}
+      <button className="open-modal-btn">
+        <svg
+          width="100"
+          height="100"
+          fill="#2a5ed4"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M18 15v3H6v-3H4v3c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-3h-2ZM7 9l1.41 1.41L11 7.83V16h2V7.83l2.59 2.58L17 9l-5-5-5 5Z"></path>
+        </svg>
+      </button>
     </div>
   );
 }
